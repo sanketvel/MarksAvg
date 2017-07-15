d367e172b25ac00ce640d60b4849cff9817d2f08
sanketvel <velhal.sanket05@gmail.com>
2017-07-15 15:24:47 +0530

    first upload

---
+++ src/AvgMarksDemo.java
@@ -0,0 +1,34 @@
+
+
+class AvgMarksDemo 
+{ 
+       int a;
+       short b;
+       long l;
+       char c = 'a';
+       byte by;
+       float f;
+       boolean bol;
+       double db;
+       
+       
+       Currency cur;
+       
+       
+       
+       // behavior : methods
+       public void name_of_method()
+       {
+    	   
+       }
+       
+        
+}
+
+
+class Currency 
+{
+	  int value;
+	  String signature;
+}
+
---
+++ src/StringManipulation.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+
+public class StringManipulation 
+{
+		
+	
+	
+	//main
+	public static void main(String args[])
+	{
+		int count = 0;
+		String s = "Hello this is awesome";
+
+		
+		try 
+		{
+		
+		   int ans = 1/0;
+		   System.out.println(ans);
+		   
+		}
+		catch(Exception ex)
+		{
+			System.out.println("Error occured." + ex );
+		}
+		
+		finally
+		{
+			// resource release
+		}
+
+		
+		
+		
+
+
+	}
+
+	
+}
